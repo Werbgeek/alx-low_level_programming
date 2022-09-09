52943cc81fb8b3e05ea2522c86caab596aad9699
Werbgeek <musiabshuaib@gmail.com>
2022-09-08 23:35:26 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,12 @@
-# include <stdio.h>
-/*
- * main - Entry point
+#include <unistd.h>
+/**
+ *main - Entry point
  *
- * Return: Always 0 (Sucess)
+ * Return: Always 1 (Success)
  */
-int main (void)
+int main(void)
 {
-putchar(and that piece of art is useful/" - Dora Korpar, 2015-10-19/n)
+char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, str1, 59);
+return (1);
 }
