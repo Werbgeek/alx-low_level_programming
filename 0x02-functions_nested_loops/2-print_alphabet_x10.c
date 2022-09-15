ccbe42db7d2c63c5cba3a7747b308da9705bbf01
Werbgeek <musiabshuaib@gmail.com>
2022-09-14 20:13:53 -0700

    print

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - entry point 
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+	
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
