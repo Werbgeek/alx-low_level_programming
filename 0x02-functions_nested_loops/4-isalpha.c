a5e5b068b25a0294a7c6a89f0de3ac53ee6fe8a3
Werbgeek <musiabshuaib@gmail.com>
2022-09-15 15:45:45 -0700

    isaplha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic 
+ *
+ * return : 0 or 1
+ */
+
+int _isalpha(int c)
+{
+	char c;
+
+	if (c >= _isalpha)
+	{
+		c = "h"
+		return (1);
+	}
+	else
+	{
+		return (0)
+	}
+}
