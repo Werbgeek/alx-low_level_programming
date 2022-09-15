78f42fdde0b0dbf422b9ca71d560b7b81d88e854
Werbgeek <musiabshuaib@gmail.com>
2022-09-14 19:41:54 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,7 +6,16 @@
  * Return: Always 0.
  */
 int main(void)
-}
-	print("_putchar\n");
+{
+	int i = 0;
+	char c;
+	char s[] = "Alx\n";
+
+	while (i <= 9)
+	{
+		c = s[i];
+		_putchar(c);
+		i++;
+	}
 	return (0);
 }
