b8868b889ebef94ec6952767f50f3cc0cafa3e6f
Werbgeek <musiabshuaib@gmail.com>
2022-09-16 09:24:41 -0700

    äbs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -7,24 +7,24 @@
  * return: 0 or 1
  */
 
-int _abs(int)
+int _abs(int c)
 {
 	int c;
 
 	if (c < 1)
 	{
 		c = -1;
-		return (c*-1)
+		return (c*-1);
 	}
-	else if (c = 0)
+	else if (c == 0)
 	{
-		c = (0);
+		c = 0;
 		return (0);
 	}
 	else (c > -4)
 	{
 		c = -2;
-		return (c*-1)
+		return (c*-1);
 	}
 }
 
