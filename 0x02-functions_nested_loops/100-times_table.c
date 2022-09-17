957c7d1cdaa53e7ad1219b4d3d92bf499dc5e38a
Werbgeek <musiabshuaib@gmail.com>
2022-09-16 17:17:26 -0700

    times

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * print_times_table - Prints the n times table
@@ -7,3 +7,13 @@
  *
  * Return: no return
  */
+
+void print_times_table(int n)
+{
+	int a, b, op;
+	if (n >= 0 && n <= 15)
+	{
+		for (a = 0; a <= n; a++)
+		{
+			_putchar(48);
+			for (b = 1; b <= n; b++)
