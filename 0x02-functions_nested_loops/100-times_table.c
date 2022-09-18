2dae9655cd08c3f26568cf671b5ee30715d8dc04
Werbgeek <musiabshuaib@gmail.com>
2022-09-18 15:50:09 -0700

    times

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,23 +17,23 @@ void print_times_table(int n)
 		{
 			for (b = 0; b <= n; b++)
 			{
-				c = a * b;d = c / 100;
-				e = (c /  10) % 10;f = (c % 100) % 10;
+				c = a * b; d = c / 100;
+				e = (c /  10) % 10; f = (c % 100) % 10;
 				if (b == 0)
 				{
 					_putchar('0');
 				}
 				else if (c < 10)
 				{
-					_putchar(' ');_putchar(' ');
+					_putchar(' '); _putchar(' ');
 					_putchar('0' + f);
 				}
 				else if (c < 100)
 				{
 					_putchar(' ');
-					_putchar('0' + e);_putchar('0' + f);
+					_putchar('0' + e) ;_putchar('0' + f);
 				}
-				else 
+				else
 				{
 					_putchar('0' + d);
 					_putchar('0' + e);
@@ -41,7 +41,7 @@ void print_times_table(int n)
 				}
 				if (b < n)
 				{
-					_putchar(',');_putchar(' ');
+					_putchar(','); _putchar(' ');
 				}
 				else
 					_putchar('\n');
