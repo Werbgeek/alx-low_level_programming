ba881abdb855dc96d37f4b0a456a49ec8daffe8e
Werbgeek <musiabshuaib@gmail.com>
2022-09-18 17:47:10 -0700

    fizz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,7 +4,7 @@
 /**
  * main - entry point
  *
- * Description: prints the numbers 1 - 100 w/Fizz for multiples of 3, Buzz for multiples of 5,and FizzBuzz for multiples of both
+ * Description: prints the numbers 1 - 100 w/Fizz for multiples of 3, Buzz
  *
  * Return: always 0
  */
